cd1950d30fb21cfc88f0f571e76178472d94317f
NaikVedhas <vedhasnaik121@gmail.com>
2025-11-03 14:35:10 +0530

    Updated Contract 2

--- Contract2.sol
+++ Contract2.sol
@@ -15,41 +15,50 @@ contract local {
     mapping(address => Life) public lives;
     uint public totalLives;
 
-    function born(string memory _name) public {
+    // Modifier: Check if life exists
+    modifier onlyAlive() {
+        require(bytes(lives[msg.sender].name).length != 0, "Bro, pehle janam toh le.");
+        _;
+    }
+
+     function born(string memory _name) public payable  {
         require(bytes(lives[msg.sender].name).length == 0, "Bro, you already have a life.");
+        require(msg.value >= 0.001 ether, "Bhai, zindagi paison se chalti hai. ~ Need at least 0.001 ETH for future.");
+
         Life storage newLife = lives[msg.sender];
         newLife.name = _name;
         newLife.age = 0;
-        totalLives++; // counts new life :)
+
+        totalLives++;
     }
 
-    function addLove(string memory _love) public {
+    function addLove(string memory _love) public onlyAlive {
         lives[msg.sender].love.push(_love);
     }
 
-    function setCareer(string memory _career) public {
+    function setCareer(string memory _career) public onlyAlive {
         lives[msg.sender].career = _career;
     }
 
-    function addSideChick(string memory _sideChick) public {
+    function addSideChick(string memory _sideChick) public onlyAlive{
         lives[msg.sender].sideChick.push(_sideChick);
     }
 
-    function addFriend(string memory _friend) public {
+    function addFriend(string memory _friend) public onlyAlive{
     lives[msg.sender].friends.push(_friend);
 }
-function birthday() public returns (string memory quote) {
+function birthday() public onlyAlive returns (string memory quote) {
     lives[msg.sender].age += 1;
     return string.concat(" Happy Birthday ", lives[msg.sender].name, "!");
 }
 
-function removeSideChick() public {
+function removeSideChick() public onlyAlive{
     if(lives[msg.sender].sideChick.length > 0){
         lives[msg.sender].sideChick.pop();
     }
 }
 
-    function getLife() public view returns (Life memory life) {
+    function getLife() public view onlyAlive returns (Life memory life) {
         return lives[msg.sender];
     }
 }
